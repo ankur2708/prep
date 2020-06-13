7113549ffc1185675b7881cbfd0a66cd5bd0caf3
ankur <ankur@ANKUR-PC>
2020-06-14 03:10:31 +0530

    union find with path compression and driver app

---
+++ prep/src/com/prep/algos/UnionFind.java
@@ -0,0 +1,58 @@
+package com.prep.algos;
+
+public class UnionFind {
+
+	private int[] points;
+	private int[] size;
+	
+	public UnionFind(int numOfPoints)
+	{
+		points = new int[numOfPoints];
+		size = new int[numOfPoints];
+		for(int i = 0; i < numOfPoints; i++)
+		{
+			points[i] = i;
+		}
+	}
+	
+	public boolean connected(int point1, int point2)
+	{
+		validate(point1, point2);
+		return getRoot(point1) == getRoot(point2);
+	}
+	
+	public void union(int point1, int point2)
+	{
+		validate(point1, point2);
+		int root1 = getRoot(point1);
+		int root2 = getRoot(point2);
+		if(root1 == root2) 
+		{
+			return;
+		}
+		if(size[root1] >= size[root2])
+		{
+			points[root1] = root2;
+		}
+		
+	}
+	
+	private int getRoot(int point)
+	{
+		while(points[point] != point)
+		{
+			points[point] = points[points[point]]; // this line not required, but it improves time complexity by path compression!!
+			point = points[point];
+		}
+		return point;
+	}
+	
+	private void validate(int point1, int point2)
+	{
+		if(point1 < 0 || point2 < 0 || point1 > points.length - 1 || point2 > points.length -1)
+		{
+			throw new IllegalArgumentException("No such point!");
+		}
+	}
+	
+}
---
+++ prep/src/com/prep/app/UnionFindApp.java
@@ -0,0 +1,21 @@
+package com.prep.app;
+
+import com.prep.algos.UnionFind;
+
+public class UnionFindApp {
+
+	public static void main(String[] args) {
+		
+		UnionFind uf = new UnionFind(10);
+		
+		uf.union(0, 1);
+		uf.union(1, 2);
+		uf.union(2, 3);
+		uf.union(1, 4);
+		uf.union(7, 9);
+		
+		System.out.println(uf.connected(2, 4));
+		
+		
+	}
+}
